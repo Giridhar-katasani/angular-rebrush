c8ed153a2f0f9a40b7282449f7c05ade2c1eab41
giridhar <giridahrreddykatasani545@gmail.com>
2024-03-15 10:41:33 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Employee_angular_application';
+  title: string = 'Employee_angular_application';
+  isShow: boolean = true;
+  isShowEnteredInput: boolean = true;
+  enteredInput: string = "";
+  userName: string = "Giridhar";
+
+  constructor() { };
+
+  ngOnInit() {
+  }
+
+  public getTitle(): string {
+    return "from the method";
+  }
+
+  public clicked(): void {
+    console.log("clicked");
+    this.isShowEnteredInput=false;
+  }
+
+  public userInput(event:any): void {
+    this.enteredInput = event.target.value;
+    this.userName = event.target.value;
+    console.log(event.target.value);
+  }
 }
